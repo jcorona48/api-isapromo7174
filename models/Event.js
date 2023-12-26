80053d12d227086a44673d565a7fac17c8d88828
Jarison Alonzo <jarisonalonzo34@gmail.com>
2023-12-25 23:00:03 -0400

    Update de fecha

--- models/Event.js
+++ models/Event.js
@@ -1,31 +1,10 @@
 import { Schema, model } from "mongoose";
 
-// Definir el esquema Usuario de la colección
 
-/* 
-    Campos:
-
-    - codigo: String (único)
-    - nombres: String (requerido)
-    - apellidos: String (requerido)
-    - Foto: String (requerido)
-    - Sexo: String (requerido)
-    - Estado Civil: String
-    - Cantidad de hijos: Number (default: 0)
-    - Fecha de nacimiento: Date (requerido)
-    - Lugar de nacimiento: String 
-    - Dirección: String
-    - TelefonoCasa: String
-    - TelefonoCelular: String 
-    - Correo: String (requerido)
-    - Ultimo grado de estudio: String (requerido)
-
-*/
 
 const eventSchema = new Schema(
     {
-        // Definir los campos del esquema
-        // El campo _id es creado automáticamente por MongoDB
+     
         codigo: {
             type: String,
             unique: true,
@@ -35,20 +14,14 @@ const eventSchema = new Schema(
             type: String,
             required: true,
         },
-        fecha: {
+        fechaInicio: {
             type: Date,
             required: true,
-            
         },
-        horaInicio: {
+
+        fechaFinal: {
             type: Date,
-            required: true,
-            
-        },
-        horaFinal: {
-            type: Date,
-            required: true,
-            
+            required: true,  
         },
         lugar: {
             type: String,
